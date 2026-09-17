996dbfb6b97f2505d4b8a0e92038e51dbcb9c479
Swapnil Ghosh <swaapnil.ghosh@gmail.com>
2026-09-17 10:29:03 +0530

    style(frontend): wrap SearchView with framer-motion opacity fade

--- frontend/src/views/SearchView.jsx
+++ frontend/src/views/SearchView.jsx
@@ -1,5 +1,15 @@
 import SearchPanel from '../components/SearchPanel'
+import { motion } from 'framer-motion'
 
 export default function SearchView() {
-  return <SearchPanel />
+  return (
+    <motion.div
+      className="w-full flex-1"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+    >
+      <SearchPanel />
+    </motion.div>
+  )
 }
